b4dd31e35674260f8fd14a1ba50886d7c8b094d1
Zoltán Baranyai <xpleedek@gmail.com>
2018-11-29 17:42:26 +0100

    Valami változott

--- .vs/Repo/v15/Keprendezo/Keprendezo/Keprendezo.cs
+++ .vs/Repo/v15/Keprendezo/Keprendezo/Keprendezo.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Keprendezo
 {
@@ -19,9 +20,9 @@ namespace Keprendezo
         //Megadjuk azt a mappát amelyből másolni szeretnénk
         private void buttonCel_Click(object sender, EventArgs e)
         {
-            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                textBoxCel.Text = folderBrowserDialog2.SelectedPath;
+                textBoxCel.Text = folderBrowserDialog1.SelectedPath;
             }
         }
         //Megadjuk azt a mappát amelybe másolni szeretnénk
@@ -39,29 +40,30 @@ namespace Keprendezo
             string forrasPath = textBoxForras.Text;
             string celPath = textBoxCel.Text;
 
-            string sourceFile = System.IO.Path.Combine(forrasPath, fileNev);
-            string destFile = System.IO.Path.Combine(celPath, fileNev);
+            string sourceFile = Path.Combine(forrasPath, fileNev);
+            string destFile = Path.Combine(celPath, fileNev);
 
             //Létrehozza a mappát ha még nem létezik
-            if (!System.IO.Directory.Exists(celPath))
+            if (!Directory.Exists(celPath))
             {
-                System.IO.Directory.CreateDirectory(celPath);
+                Directory.CreateDirectory(celPath);
             }
 
-            //Felülírja az azonos nevő fáljokat
-            System.IO.File.Copy(sourceFile, destFile, true);
+            //Valami 
+            //Felülírja az azonos nevő fáljt
+            //File.Copy(sourceFile, destFile, true);
 
-            if (System.IO.Directory.Exists(forrasPath))
+            if (Directory.Exists(forrasPath))
             {
-                string[] files = System.IO.Directory.GetFiles(forrasPath);
+                string[] files = Directory.GetFiles(forrasPath);
 
                 // Copy the files and overwrite destination files if they already exist.
                 foreach (string s in files)
                 {
                     // Use static Path methods to extract only the file name from the path.
-                    fileNev = System.IO.Path.GetFileName(s);
-                    destFile = System.IO.Path.Combine(celPath, fileNev);
-                    System.IO.File.Copy(s, destFile, true);
+                    fileNev = Path.GetFileName(s);
+                    destFile = Path.Combine(celPath, fileNev);
+                    File.Copy(s, destFile, true);
                 }
             }
         }
